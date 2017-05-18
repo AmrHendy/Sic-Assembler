b5396f1a2c7a89636423dc86807078025b8d0e79
MuhammedKhamis <muhammedessam10@gmail.com>
2017-05-18 22:49:28 +0200

    EQUte in Line Address Generator :dancer:

--- src/IntermediateFile/LineAddressGenerator.java
+++ src/IntermediateFile/LineAddressGenerator.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import data.Data;
 import exception.StatementException;
 import statement.IStatement;
 import statement.Operation;
@@ -12,96 +13,151 @@ import tools.Checker;
 
 public class LineAddressGenerator {
 
-	private List<String> intermediateContent;
-	private int currentLineAddress, nextLineAddress;
-	public static boolean error;
-	
+    private List<String> intermediateContent;
+    private int currentLineAddress, nextLineAddress;
+    public static boolean error;
+    /**********************Flag for error in EQU***********************/
+    private boolean errorInEqu = false;
 
-	public LineAddressGenerator() {
-		intermediateContent = new ArrayList<String>();
-		currentLineAddress = Checker.convertFromHexaToDeca(new String("000000"));
-		nextLineAddress = currentLineAddress;
-		error = false;
-	}
+    public LineAddressGenerator() {
+        intermediateContent = new ArrayList<String>();
+        currentLineAddress = Checker.convertFromHexaToDeca(new String("000000"));
+        nextLineAddress = currentLineAddress;
+        error = false;
+    }
 
-	public void setInitialAddress(String startOperand) {
-		currentLineAddress = Checker.convertFromHexaToDeca(startOperand.substring(3, startOperand.length() - 1));
-		nextLineAddress = currentLineAddress;
-	}
+    public void setInitialAddress(String startOperand) {
+        currentLineAddress = Checker.convertFromHexaToDeca(startOperand.substring(3, startOperand.length() - 1));
+        nextLineAddress = currentLineAddress;
+    }
 
-	public String appendStatement(String originalStatement, String statementContent[], IStatement statement)
-			throws StatementException {
-		if (statement instanceof Operation) {
-			nextLineAddress = calculateNextLineAddress();
-		} else {
-			nextLineAddress = calculateNextLineAddress(statementContent);
-		}
-		// 02_generate current line for intermediate file
-		if (currentLineAddress >= Math.pow(2, 15) || nextLineAddress >= Math.pow(2, 15)) {
-			nextLineAddress = currentLineAddress;
-			throw new StatementException("Out of Memory Range");
-		}
-		String address = Checker.getHexaFromDecimal(String.valueOf(currentLineAddress));
-		String line = address + "    " + originalStatement;
-		intermediateContent.add(line);
-		currentLineAddress = nextLineAddress;
-		return address;
-	}
-	
-	public String appendStatement(String literalStatement, int literalLength) throws StatementException{
-		if (currentLineAddress >= Math.pow(2, 15) || nextLineAddress >= Math.pow(2, 15)) {
-			nextLineAddress = currentLineAddress;
-			throw new StatementException("Out of Memory Range");
-		}
-		String address = Checker.getHexaFromDecimal(String.valueOf(currentLineAddress));
-		String line = address + "    " + literalStatement;
-		intermediateContent.add(line);
-		nextLineAddress=currentLineAddress+literalLength;
-		currentLineAddress = nextLineAddress;
-		return address;
-	}
+    public String appendStatement(String originalStatement, String statementContent[], IStatement statement)
+            throws StatementException {
+        if (statement instanceof Operation) {
+            nextLineAddress = calculateNextLineAddress();
+        } else {
+            nextLineAddress = calculateNextLineAddress(statementContent);
+        }
+        // 02_generate current line for intermediate file
+        if (currentLineAddress >= Math.pow(2, 15) || nextLineAddress >= Math.pow(2, 15)) {
+            nextLineAddress = currentLineAddress;
+            throw new StatementException("Out of Memory Range");
+        }
+        String address = Checker.getHexaFromDecimal(String.valueOf(currentLineAddress));
+        String line = address + "    " + originalStatement;
+        intermediateContent.add(line);
+        currentLineAddress = nextLineAddress;
+        return address;
+    }
 
-	public void appendError(String originalStatement, String errorMessege) {
-		error = true;
-		intermediateContent.add(new String(". ****" + errorMessege));
-		intermediateContent
-				.add(Checker.getHexaFromDecimal(String.valueOf(currentLineAddress)) + "    " + originalStatement);
-	}
+    public String appendStatement(String literalStatement, int literalLength) throws StatementException {
+        if (currentLineAddress >= Math.pow(2, 15) || nextLineAddress >= Math.pow(2, 15)) {
+            nextLineAddress = currentLineAddress;
+            throw new StatementException("Out of Memory Range");
+        }
+        String address = Checker.getHexaFromDecimal(String.valueOf(currentLineAddress));
+        String line = address + "    " + literalStatement;
+        intermediateContent.add(line);
+        nextLineAddress = currentLineAddress + literalLength;
+        currentLineAddress = nextLineAddress;
+        return address;
+    }
 
-	public void appendComment(String originalStatement) {
-		intermediateContent.add(new String("          " + originalStatement));
-	}
+    public void appendError(String originalStatement, String errorMessege) {
+        error = true;
+        intermediateContent.add(new String(". ****" + errorMessege));
+        intermediateContent
+                .add(Checker.getHexaFromDecimal(String.valueOf(currentLineAddress)) + "    " + originalStatement);
+    }
 
-	public List<String> getContent() {
-		return this.intermediateContent;
-	}
+    public void appendComment(String originalStatement) {
+        intermediateContent.add(new String("          " + originalStatement));
+    }
 
-	private int calculateNextLineAddress(String currentStatement[]) {
-		if (currentStatement[1].equalsIgnoreCase("RESW")) {
-			nextLineAddress += 3 * Integer.parseInt(currentStatement[2]);
-		} else if (currentStatement[1].equalsIgnoreCase("RESB")) {
-			nextLineAddress += Integer.parseInt(currentStatement[2]);
-		} else if (currentStatement[1].equalsIgnoreCase("WORD")) {
-			nextLineAddress += 3;
-		} else if (currentStatement[1].equalsIgnoreCase("BYTE")) {
-			String chaPattern = "(C*?)(')(.+)(')";
-			Matcher queryMatcher = Pattern.compile(chaPattern).matcher(currentStatement[2]);
-			if (queryMatcher.matches()) {
-				String value = queryMatcher.group(3);
-				nextLineAddress += value.length();
-			}
-			String hexPattern = "(X*?)(')(.+)(')";
-			queryMatcher = Pattern.compile(hexPattern).matcher(currentStatement[2]);
-			if (queryMatcher.matches()) {
-				String value = queryMatcher.group(3);
-				nextLineAddress += Math.ceil(value.length() * 1. / 2.);
-			}
-		}
-		// ORG EQUATE
-		return nextLineAddress;
-	}
+    public List<String> getContent() {
+        return this.intermediateContent;
+    }
 
-	private int calculateNextLineAddress() {
-		return nextLineAddress + 3;
-	}
+    private int calculateNextLineAddress(String currentStatement[]) {
+        if (currentStatement[1].equalsIgnoreCase("RESW")) {
+            nextLineAddress += 3 * Integer.parseInt(currentStatement[2]);
+        } else if (currentStatement[1].equalsIgnoreCase("RESB")) {
+            nextLineAddress += Integer.parseInt(currentStatement[2]);
+        } else if (currentStatement[1].equalsIgnoreCase("WORD")) {
+            nextLineAddress += 3;
+        } else if (currentStatement[1].equalsIgnoreCase("BYTE")) {
+            String chaPattern = "(C*?)(')(.+)(')";
+            Matcher queryMatcher = Pattern.compile(chaPattern).matcher(currentStatement[2]);
+            if (queryMatcher.matches()) {
+                String value = queryMatcher.group(3);
+                nextLineAddress += value.length();
+            }
+            String hexPattern = "(X*?)(')(.+)(')";
+            queryMatcher = Pattern.compile(hexPattern).matcher(currentStatement[2]);
+            if (queryMatcher.matches()) {
+                String value = queryMatcher.group(3);
+                nextLineAddress += Math.ceil(value.length() * 1. / 2.);
+            }
+            // EQUATE
+        } else if (currentStatement[1].equalsIgnoreCase("EQU")) {
+            int sign = 1;
+            int start = 0;
+            if (currentStatement[2].charAt(0) == '-') {
+                sign = -1;
+                start = 1;
+            }
+            int value = totalValue(currentStatement[2], start, sign);
+            if(errorInEqu){
+                error=true;   
+            }else{
+                Data.symbolTable.put(currentStatement[0].toLowerCase(), String.valueOf(value));
+            }
+        }
+        // ORG
+        return nextLineAddress;
+    }
+
+    private int totalValue(String line, int start, int sign) {
+        StringBuilder tmp = new StringBuilder();
+        int i = start;
+        for (; i < line.length(); i++) {
+            if (line.charAt(i) == '+') {
+                sign = 1;
+                i++;
+                break;
+            } else if (line.charAt(i) == '-') {
+                sign = -1;
+                i++;
+                break;
+            }
+            tmp.append(line.charAt(i));
+        }
+        return getValue(tmp.toString()) * sign + totalValue(line, start, sign);
+    }
+
+    private int getValue(String symbol) {
+        int value = -1;
+        if (Data.symbolTable.containsKey(symbol)) {
+            value = Checker.convertFromHexaToDeca(Data.symbolTable.get(symbol));
+        } else if (isDecimalNumber(symbol)) {
+            value = Integer.valueOf(symbol);
+        }
+        if(value == -1){
+            errorInEqu = true;
+        }
+        return value;
+    }
+
+    private boolean isDecimalNumber(String line) {
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) > '9' || line.charAt(i) < '0') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int calculateNextLineAddress() {
+        return nextLineAddress + 3;
+    }
 }
